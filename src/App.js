3d6cd1c2381b6e803b48b5cceaac37dea6795cd9
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-27 19:03:59 -0500

    wrapped app in ProductContext.Provider and simplified the return statement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+
+import ProductContext from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-
-export const CartContext = createContext();
 
 function App() {
 	const [products] = useState(data);
@@ -20,16 +20,18 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
